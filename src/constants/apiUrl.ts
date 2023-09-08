719c4e299ae59b318aa33f74e8eaab1e00ac7f12
prefer_choi <prefer_choi@naver.com>
2023-09-08 18:04:10 +0900

    fix: test API_BASE_URL 전환

--- src/constants/apiUrl.ts
+++ src/constants/apiUrl.ts
@@ -1,2 +1,1 @@
-// export const API_BASE_URL = "https://wanted-backend-week3.vercel.app/";
-export const API_BASE_URL = "http://localhost:4000/";
+export const API_BASE_URL = "https://wanted-backend-week3.vercel.app/";
